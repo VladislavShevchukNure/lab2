df1ce86e3407c9946065ed820044c7165a4cdb23
VladislavShevchukNure <128866805+VladislavShevchukNure@users.noreply.github.com>
2023-04-24 18:41:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "arn:aws:sts::367636258589:assumed-role/LabRole/i-00a0ff05dfd4052ec"
+    bucket         = "terraform-backend-shevchuk"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "backendtable"
